d94ab863562d01071b0a094cc8dd104389451a7b
Thomas Bornhorst <thomasbornhorst@MacBook-Pro-10.local>
2022-02-14 21:08:05 -0500

    added reset button + small changes

--- main.js
+++ main.js
@@ -2,10 +2,101 @@ let map;
 var mapLoaded = false;
 let jsonFeat;
 
+const startPos = { lat: 42.318870, lng: -71.588521 };
+let initOpacity = 0.10;
+let hoverOpacity = 0.30;
+let clickOpacity = 0.45;
+
+var isClicked = false;
+
+initFirebase();
+
+class TownDoc {
+    constructor(id, data){
+        this.id = id;
+        this.data = data;
+    }
+}
+
+async function initFirebase() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCUb8ha9pqPsqU2wUxHneGMHt3Ne774e-g",
+        authDomain: "map-project-340905.firebaseapp.com",
+        projectId: "map-project-340905",
+        storageBucket: "map-project-340905.appspot.com",
+        messagingSenderId: "796916181236",
+        appId: "1:796916181236:web:cd2ea174b71e07b54016a1",
+        measurementId: "G-NG3MZ5ZMCB"
+      };
+    
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    
+    //   firebase.auth.Auth.Persistence.LOCAL;
+    
+      const db = firebase.firestore();
+      const docs = await db.collection('towns').get();
+      docs.forEach(function(doc) {
+        var id = doc.id;
+        var data = doc.data();
+        const town = new TownDoc(id, data);
+        // console.log(town);
+      });
+}
+
+function AddHomeControl(controlDiv, map) {
+    // Set CSS for the control border.
+    const controlUI = document.createElement("div");
+  
+    controlUI.style.backgroundColor = "#fff";
+    controlUI.style.border = "2px solid #fff";
+    controlUI.style.borderRadius = "1px";
+    controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+    controlUI.style.cursor = "pointer";
+    controlUI.style.marginTop = "8px";
+    controlUI.style.marginBottom = "8px";
+    controlUI.style.marginLeft = "8px";
+    controlUI.style.textAlign = "center";
+    controlUI.style.color = "rgb(100, 100, 100)";
+    controlUI.title = "Click to reset view";
+    controlUI.style.backgroundImage = "/images/cdp_logo.jpeg";
+    controlDiv.appendChild(controlUI);
+  
+    const controlIcon = document.createElement("i");
+    controlIcon.className = "fa fa-home fa-10x";
+    controlIcon.style = "font-size: 35px;";
+    controlUI.appendChild(controlIcon);
+
+
+    // Set CSS for the control interior.
+    // const controlText = document.createElement("div");
+  
+    // controlText.style.color = "rgb(25,25,25)";
+    // controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+    // controlText.style.fontSize = "16px";
+    // controlText.style.lineHeight = "38px";
+    // controlText.style.paddingLeft = "5px";
+    // controlText.style.paddingRight = "5px";
+    // controlText.innerHTML = "Home";
+    // controlUI.appendChild(controlText);
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener("click", () => {
+        isClicked = false;
+        map.setZoom(9);
+        map.setCenter(startPos);
+    });
+
+    controlUI.addEventListener("mouseover", () => {
+        controlUI.style.color = "rgb(52, 52, 52)";
+    });
+
+    controlUI.addEventListener("mouseout", () => {
+        controlUI.style.color = "rgb(100, 100, 100)";
+    });
+}
+
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const startPos = { lat: 42.318870, lng: -71.588521 };
     // The map, centered at Uluru
     var defaultZoom = 9;
     map = new google.maps.Map(document.getElementById("map"), {
@@ -25,21 +116,13 @@ function initMap() {
         mapTypeControl: false,
     });
 
+    const controlDiv = document.createElement("div");
+
+    AddHomeControl(controlDiv, map);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(controlDiv);
+
     // createSwitches();
     switchChanged();
-
-    google.maps.Polygon.prototype.getBounds = function () {
-        var bounds = new google.maps.LatLngBounds();
-        var paths = this.getPaths();
-        var path;
-        for (var i = 0; i < paths.getLength(); i++) {
-            path = paths.getAt(i);
-            for (var ii = 0; ii < path.getLength(); ii++) {
-                bounds.extend(path.getAt(ii));
-            }
-        }
-        return bounds;
-    }
 
     google.maps.event.addListener(map, 'idle', function () {
         if (!mapLoaded) {
@@ -75,14 +158,15 @@ function initMap() {
         return {
             fillColor: color,
             strokeWeight: 1,
-            fillOpacity: 0.35
+            fillOpacity: initOpacity,
         };
     });
 
     map.data.addListener("mouseover", (event) => {
-        map.data.overrideStyle(event.feature, { fillOpacity: 0.5 });
-        document.getElementById("info-box").textContent =
-            event.feature.getProperty("TOWN");
+        map.data.overrideStyle(event.feature, { fillOpacity: hoverOpacity });
+        if(isClicked)
+            return;
+        updateSelectedTown(event);
     });
 
     map.data.addListener("mouseout", (event) => {
@@ -90,14 +174,20 @@ function initMap() {
     });
 
     map.data.addListener("click", (event) => {
+        isClicked = true;
         var bounds = new google.maps.LatLngBounds();
-        map.data.overrideStyle(event.feature, { fillOpacity: 0.75 });
-        document.getElementById("info-box").textContent =
-            event.feature.getProperty("TOWN");
+        map.data.overrideStyle(event.feature, { fillOpacity: clickOpacity});
+        updateSelectedTown(event);
         event.feature.getGeometry().forEachLatLng(latLng => bounds.extend(latLng));
         map.fitBounds(bounds, 0);
     });
 
+}
+
+function updateSelectedTown(event) {
+    var townName = event.feature.getProperty("TOWN");
+    document.getElementById("info-box").textContent = townName;
+    document.getElementById("town-header").textContent = townName;
 }
 
 //search bar
